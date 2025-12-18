d160b20b4e89f2651430c62d0997a2081a081505
Michael <m.zakka@tgmena.com>
2025-12-18 17:58:47 +0300

    client edits final

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,11 +3,14 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./Footer.module.css";
-import logo from "../assets/logo.png";
+import ministryLogo from "../assets/وزارة التجارة.svg";
+import intellectualPropertyLogo from "../assets/الهيئة السعودية للملكية الفكرية.svg";
+import mediaRegulationLogo from "../assets/الهيئة العامة لتنظيم الإعلام.svg";
+import zakatTaxLogo from "../assets/هيئة الزكاة والضريبة والجمارك.svg";
 
 const translations = {
   en: {
-    brandDescription: "Art & Experience.. Driven by Performance.",
+    brandDescription: "Sam Advertising Company",
     brandInfo: "Professional Advertising Solutions & Signage",
     established: "Established since 2018",
     quickLinks: "Quick Links",
@@ -22,7 +25,7 @@ const translations = {
     copyright: "All rights reserved.",
   },
   ar: {
-    brandDescription: "الفن والخبرة.. مدفوع بالأداء.",
+    brandDescription: "شركة سام للإعلان",
     brandInfo: "حلول إعلانية احترافية ولافتات",
     established: "تأسست منذ 2018",
     quickLinks: "روابط سريعة",
@@ -71,14 +74,6 @@ export default function Footer() {
         <div className={styles.grid}>
           {/* Brand Section */}
           <div className={styles.brandSection}>
-            <div className={styles.brandLogo}>
-              <Image 
-                src={logo}
-                alt="SAM SIGN"
-                className={styles.logoImage}
-                priority
-              />
-            </div>
             <p className={styles.brandDescription}>
               {t.brandDescription}
             </p>
@@ -88,81 +83,86 @@ export default function Footer() {
               {t.established}
             </p>
           </div>
+        </div>
 
-          {/* Quick Links */}
-          <div>
-            <h4 className={styles.sectionTitle}>{t.quickLinks}</h4>
-            <ul className={styles.linksList}>
-              <li>
-                <a
-                  href="#philosophy"
-                  className={styles.link}
-                >
-                  {t.philosophy}
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#partners"
-                  className={styles.link}
-                >
-                  {t.partners}
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#vision"
-                  className={styles.link}
-                >
-                  {t.visionValues}
-                </a>
-              </li>
-            </ul>
+        {/* Authority Logos Section */}
+        <div className={styles.ministrySection}>
+          {/* Ministry of Commerce Logo */}
+          <div className={styles.ministryItem}>
+            <Image 
+              src={ministryLogo}
+              alt="وزارة التجارة"
+              width={120}
+              height={120}
+              className={styles.ministryLogo}
+            />
+            <p className={styles.ministryNumber}>7037099914</p>
           </div>
 
-          {/* Contact Info */}
-          <div>
-            <h4 className={styles.sectionTitle}>{t.contact}</h4>
-            <ul className={styles.contactList}>
-              <li>
-                <span className={styles.contactLabel}>{t.phone}</span> +966 XX XXX XXXX
-              </li>
-              <li>
-                <span className={styles.contactLabel}>{t.email}</span> info@samsign.com
-              </li>
-              <li>
-                <span className={styles.contactLabel}>{t.address}</span> [Your Address]
-              </li>
-            </ul>
+          {/* Intellectual Property Authority Logo */}
+          <div className={styles.ministryItem}>
+            <Image 
+              src={intellectualPropertyLogo}
+              alt="الهيئة العامة للملكية الفكرية"
+              width={120}
+              height={120}
+              className={styles.ministryLogo}
+            />
+            <p className={styles.ministryNumber}>TM-01-00-14126-24</p>
           </div>
 
-          {/* Follow Us */}
-          <div>
-            <h4 className={styles.sectionTitle}>{t.followUs}</h4>
-            <div className={styles.socialLinks}>
-              <a
-                href="#"
-                className={styles.socialLink}
-                aria-label="LinkedIn"
-              >
-                <span className={styles.socialLinkText}>in</span>
-              </a>
-              <a
-                href="#"
-                className={styles.socialLink}
-                aria-label="Instagram"
-              >
-                <span className={styles.socialLinkText}>ig</span>
-              </a>
-              <a
-                href="#"
-                className={styles.socialLink}
-                aria-label="Twitter"
-              >
-                <span className={styles.socialLinkText}>X</span>
-              </a>
-            </div>
+          {/* Media Regulation Authority Logo */}
+          <div className={styles.ministryItem}>
+            <Image 
+              src={mediaRegulationLogo}
+              alt="الهيئة العامة لتنظيم الاعلام"
+              width={120}
+              height={120}
+              className={styles.ministryLogo}
+            />
+            <p className={styles.ministryNumber}>151813</p>
           </div>
+
+          {/* Zakat and Tax Authority Logo */}
+          <div className={styles.ministryItem}>
+            <Image 
+              src={zakatTaxLogo}
+              alt="هيئة الزكاة والضريبة"
+              width={120}
+              height={120}
+              className={styles.ministryLogo}
+            />
+            <p className={styles.ministryNumber}>311874026800003</p>
+          </div>
+        </div>
+
+        {/* Contact Info */}
+        <div className={styles.contactSection}>
+          <ul className={styles.contactList}>
+            <li>
+              <span className={styles.contactLabel}>{t.phone}</span>
+              <a href="tel:+966536211111" className={styles.contactValue} dir="ltr">+966 5 362 11111</a>
+            </li>
+            <li>
+              <span className={styles.contactLabel}>{t.email}</span>
+              <a href="mailto:info@samsign.sa" className={styles.contactValue} dir="ltr">info@samsign.sa</a>
+            </li>
+            <li>
+              <span className={styles.contactLabel}>
+                <svg 
+                  width="16" 
+                  height="16" 
+                  viewBox="0 0 24 24" 
+                  fill="currentColor" 
+                  className={styles.instagramIcon}
+                  aria-hidden="true"
+                >
+                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                </svg>
+              </span>
+              <a href="https://instagram.com/SamSign.sa" target="_blank" rel="noopener noreferrer" className={styles.contactValue} dir="ltr">SamSign.sa</a>
+            </li>
+          </ul>
         </div>
 
         {/* Copyright */}
